fbfaccdec3b8b0cceb0ce5b77c92d74c09d48b56
haptorx9 <haptorx9@hotmail.com>
2020-05-08 20:42:53 -0300

    logica da peça rook, implementação do metodo isThereOpponentPiece na classPiece e validação do targetPosition

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -17,6 +17,7 @@ public class Rook extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
 		return mat;
 	}
 
